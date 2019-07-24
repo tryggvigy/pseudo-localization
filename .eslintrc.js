4657e00ad869887fca357110e236378b456657a7
Tryggvi Gylfason <tryggvi@spotify.com>
2019-07-24 16:47:41 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    // For Jest tests
+    describe: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
